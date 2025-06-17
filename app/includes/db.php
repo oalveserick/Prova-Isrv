442f83cca7accca63758f5079fd5ee786fafa917
Erick Alves <88843680+oalveserick@users.noreply.github.com>
2025-06-17 09:19:08 -0300

    1.2

--- app/includes/db.php
+++ app/includes/db.php
@@ -2,7 +2,7 @@
 require_once 'config.php';
 
 try {
-    $pdo = new PDO('mysql:host= db' . ';dbname=' . MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD);
+    $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro ao conectar ao banco: " . $e->getMessage());
